16dfbe601be796d980de69fa635326d89065039b
geomgle <eunjin.sul@gmail.com>
2022-05-07 12:54:12 +0900

    _

--- vim.py
+++ vim.py
@@ -6,21 +6,22 @@ from clipboard import copy
 from config import config
 from Xlib import X
 
-def open_vim(self, compile_latex):
-    f = tempfile.NamedTemporaryFile(mode='w+', delete=False, suffix='.tex')
 
-    f.write('$$')
+def open_vim(self, compile_latex):
+    f = tempfile.NamedTemporaryFile(mode="w+", delete=False, suffix=".tex")
+
+    f.write("$$")
     f.close()
 
-    config['open_editor'](f.name)
+    config["open_editor"](f.name)
 
     latex = ""
-    with open(f.name, 'r') as g:
+    with open(f.name, "r") as g:
         latex = g.read().strip()
 
     os.remove(f.name)
 
-    if latex != '$$':
+    if latex != "$$":
         if not compile_latex:
             svg = f"""<?xml version="1.0" encoding="UTF-8" standalone="no"?>
             <svg>
@@ -30,28 +31,26 @@ def open_vim(self, compile_latex):
             </svg> """
             copy(svg, target=TARGET)
         else:
-            m = tempfile.NamedTemporaryFile(mode='w+', delete=False)
-            m.write(config['latex_document'](latex))
+            m = tempfile.NamedTemporaryFile(mode="w+", delete=False)
+            m.write(config["latex_document"](latex))
             m.close()
 
             working_directory = tempfile.gettempdir()
             subprocess.run(
-                ['pdflatex', m.name],
+                ["pdflatex", "--interaction=batchmode", m.name],
                 cwd=working_directory,
                 stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL
+                stderr=subprocess.DEVNULL,
             )
 
             subprocess.run(
-                ['pdf2svg', f'{m.name}.pdf', f'{m.name}.svg'],
-                cwd=working_directory
+                ["pdf2svg", f"{m.name}.pdf", f"{m.name}.svg"], cwd=working_directory
             )
 
-            with open(f'{m.name}.svg') as svg:
+            with open(f"{m.name}.svg") as svg:
                 subprocess.run(
-                    ['xclip', '-selection', 'c', '-target', TARGET],
-                    stdin=svg
+                    ["xclip", "-selection", "c", "-target", TARGET], stdin=svg
                 )
 
-        self.press('v', X.ControlMask)
-    self.press('Escape')
+        self.press("v", X.ControlMask)
+    self.press("Escape")
